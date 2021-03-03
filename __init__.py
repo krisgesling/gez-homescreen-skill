4ec54e05cc4cb56b44801e3b2b41d87a77eac31a
Kris Gesling <kris.gesling@mycroft.ai>
2021-03-03 02:24:57 +0000

    gui.clear not essential

--- __init__.py
+++ __init__.py
@@ -8,7 +8,6 @@ class GezHomescreen(MycroftSkill):
 
     @resting_screen_handler("Gez's personal homescreen")
     def handle_homescreen_gez(self, message):
-        self.gui.clear()
         halloween_img = "https://mycroft.ai/wp-content/uploads/2019/02/haloween-Mark-I.png"
         self.gui.show_image(halloween_img)
 
